58767981f27d766f27f4a3ba06a97b1639c02052
Edeno Luiz Scherer <edenoscherer@gmail.com>
2017-08-15 23:24:38 -0300

    update tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,22 +1,18 @@
-import { IntroPage } from './../intro/intro';
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
-
+import { HomePage } from './../home/home';
 import { FeedPage } from './../feed/feed';
 
 
 @Component({
-  templateUrl: 'tabs.html'
+	templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = FeedPage;
+	tab1Root = HomePage;
+	tab2Root = FeedPage;
 
-  constructor() {
-
-  }
+	constructor() {
+	}
+	
 }
